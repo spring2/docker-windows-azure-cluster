2a83d141ea54a5e54c5345a8521206251750dbba
Cort A. Schaefer <cort@eschaefer.net>
2017-08-09 08:19:18 -0600

    add notes

--- containerConfig.sh
+++ containerConfig.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# to see output:
+# ls /var/lib/waagent/custom-script/download/0/
+# cat /var/log/azure/custom-script/handler.log
+
 # https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-convenience-script
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
